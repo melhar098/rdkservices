c9ce1b097b9505b1de6a1f874af320486582e06b
melhar098 <Mahmoud_El-haron@comcast.com>
2024-04-05 01:56:21 -0400

    DELIA-61725: [SECVULN] Thunder Services - SystemServices - User Controlled Upload URL
    Reason for change: Deprecating uploadLogs API
    Test Procedure: Confirm API is deprecated and that it uses uploadLogsAsync instead when called
    Risks: Low
    Priority: P1
    
    Signed-off-by: melhar098 <Mahmoud_El-haron@comcast.com>

--- SystemServices/SystemServices.cpp
+++ SystemServices/SystemServices.cpp
@@ -66,7 +66,7 @@
 using namespace std;
 
 #define API_VERSION_NUMBER_MAJOR 2
-#define API_VERSION_NUMBER_MINOR 1
+#define API_VERSION_NUMBER_MINOR 2
 #define API_VERSION_NUMBER_PATCH 3
 
 #define MAX_REBOOT_DELAY 86400 /* 24Hr = 86400 sec */
@@ -467,7 +467,7 @@ namespace WPEFramework {
             registerMethod(_T("getWakeupReason"), &SystemServices::getWakeupReason, this);
             registerMethod(_T("getLastWakeupKeyCode"), &SystemServices::getLastWakeupKeyCode, this);
 #endif
-            registerMethod("uploadLogs", &SystemServices::uploadLogs, this);
+            registerMethod("uploadLogs", &SystemServices::uploadLogsAsync, this);
 
             registerMethod("uploadLogsAsync", &SystemServices::uploadLogsAsync, this);
             registerMethod("abortLogUpload", &SystemServices::abortLogUpload, this);
@@ -4463,6 +4463,8 @@ namespace WPEFramework {
         /***
          * @brief : upload STB logs to the specified URL.
          * @param1[in] : url::String
+         *
+         * uploadLogs API was deprecated. Any calls to this API will use uploadLogsAsync.
          */
         uint32_t SystemServices::uploadLogs(const JsonObject& parameters, JsonObject& response)
         {
